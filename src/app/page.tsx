37f2f1cd325930d66235ca0d2b237e8d0b1d5c34
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-12-18 19:29:31 -0300

    fix banner height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,39 @@
 import { getProducts } from "@/client/products";
 import MainWrapper from "@/components/home/mainWrapper";
 import { Box, Container } from "@mui/material";
-import banner from "./../assets/images/banner.png"
+import banner from "./../assets/images/banner.png";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 const getData = async () => {
   try {
     const { data } = await getProducts();
 
-    return {products: data.products, totalPages: data.totalPages};
+    return { products: data.products, totalPages: data.totalPages };
   } catch (error: any) {
-    console.log("error", error)
-    throw "error"
+    console.log("error", error);
+    throw "error";
   }
 };
 export default async function Home() {
-  const {products, totalPages} = await getData();
+  const { products, totalPages } = await getData();
   return (
     <Container sx={{ marginY: 6 }}>
-      <Box sx={{maxWidth: '100%', marginBottom: 2, borderRadius: 10, overflow: 'hidden'}}>
-      <img src={banner.src} alt="banner" style={{ width: '100%', maxWidth: '100%', objectFit: 'contain', maxHeight: 200}} />
-
+      <Box
+        sx={{
+          maxWidth: "100%",
+          marginBottom: 2,
+          borderRadius: 10,
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src={banner.src}
+          alt="banner"
+          style={{ width: "100%", maxWidth: "100%", objectFit: "contain" }}
+        />
       </Box>
-        <MainWrapper data={products} totalPages={totalPages} />
+      <MainWrapper data={products} totalPages={totalPages} />
     </Container>
   );
 }
