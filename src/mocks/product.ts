a1810926179bd83ce8ebd9739bde47e9475f1acd
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-12-28 16:19:59 -0300

    comment products mock

--- src/mocks/product.ts
+++ src/mocks/product.ts
@@ -1,46 +1,46 @@
-import { Product } from "@/interfaces/products";
+// import { Product } from "@/interfaces/products";
 
-export const products: Product[] = [
-  {
-    _id: "1",
-    name: "producto 1",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra.",
-    price: 250,
-    discount: 10,
-    stock: 10,
-    images: [{ url: "https://swiperjs.com/demos/images/nature-1.jpg" }],
-    mainImage: "https://swiperjs.com/demos/images/nature-1.jpg",
-    status: {
-      available: true,
-    },
-  },
-  {
-    _id: "2",
-    name: "producto 2",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra.",
-    price: 250,
-    discount: 10,
-    stock: 10,
-    images: [{ url: "https://swiperjs.com/demos/images/nature-1.jpg" }],
-    mainImage: "https://swiperjs.com/demos/images/nature-1.jpg",
-    status: {
-      available: true,
-    },
-  },
-  {
-    _id: "3",
-    name: "producto 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra.",
-    price: 250,
-    discount: 10,
-    stock: 10,
-    images: [{ url: "https://swiperjs.com/demos/images/nature-1.jpg" }],
-    mainImage: "https://swiperjs.com/demos/images/nature-1.jpg",
-    status: {
-      available: true,
-    },
-  },
-];
+// export const products: Product[] = [
+//   {
+//     _id: "1",
+//     name: "producto 1",
+//     description:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra.",
+//     price: 250,
+//     discount: 10,
+//     stock: 10,
+//     images: [{ url: "https://swiperjs.com/demos/images/nature-1.jpg" }],
+//     mainImage: "https://swiperjs.com/demos/images/nature-1.jpg",
+//     status: {
+//       available: true,
+//     },
+//   },
+//   {
+//     _id: "2",
+//     name: "producto 2",
+//     description:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra.",
+//     price: 250,
+//     discount: 10,
+//     stock: 10,
+//     images: [{ url: "https://swiperjs.com/demos/images/nature-1.jpg" }],
+//     mainImage: "https://swiperjs.com/demos/images/nature-1.jpg",
+//     status: {
+//       available: true,
+//     },
+//   },
+//   {
+//     _id: "3",
+//     name: "producto 3",
+//     description:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec laoreet rutrum libero sed pharetra.",
+//     price: 250,
+//     discount: 10,
+//     stock: 10,
+//     images: [{ url: "https://swiperjs.com/demos/images/nature-1.jpg" }],
+//     mainImage: "https://swiperjs.com/demos/images/nature-1.jpg",
+//     status: {
+//       available: true,
+//     },
+//   },
+// ];
