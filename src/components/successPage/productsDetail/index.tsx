c418597a7c1d0369c92e3a3693dbb1fd3baff791
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2025-01-03 00:07:12 -0300

    [add] wsp message

--- src/components/successPage/productsDetail/index.tsx
+++ src/components/successPage/productsDetail/index.tsx
@@ -14,13 +14,27 @@ interface Props {
   products: any[]
 }
 export default function ProductsDetail({products}: Props) {
+
+  const WhatsAppLinkWithExternalURL = () => {
+    const phoneNumber = "513416694296"; // Número con código de país, sin símbolos
+    const message = `Hola. Acabo de realizar una compra (Nº de pedido: 123131313132131). Compré el producto "Zapatos Verdes" en cantidad de 2 unidades. Necesito coordinar el envío a mi dirección. Por favor, indícame cómo proceder.`;
+  
+    // Construir enlace de WhatsApp
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+  
+    return (
+      <a href={whatsappUrl} target="_blank" rel="noopener noreferrer">
+        Enviar mensaje a WhatsApp con enlace externo
+      </a>
+    );
+  };
   return (
     <>
       {products
         .map((product: any) => (
           <>
             <Box key={product.details[0]._id} sx={{ marginBottom: 2 }}>
-              <CartItemCard hasDelete={false} data={{quantity: product.quantity, ...product.details[0], price: product.data.price}} />
+              <CartItemCard hasDelete={false} data={{quantity: product.quantity, color:product.data.color, size: product.data.size, ...product.details[0], price: product.data.price}} />
             </Box>
           </>
         ))}
@@ -42,7 +56,7 @@ export default function ProductsDetail({products}: Props) {
         spacing={1}
         sx={{ marginTop: 2 }}
       >
-        <Button variant="contained">Coordinar envío</Button>
+        <Button variant="contained" onClick={WhatsAppLinkWithExternalURL}>Coordinar envío</Button>
       </Stack>
       {products.length === 0 && <Typography>El carrito está vacío</Typography>}
     </>
